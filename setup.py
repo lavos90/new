f91c8de6679ed1544dbf315f57a7893f8d75259f
Yadnier Cervera <yadnierv@gmail.com>
2024-06-29 09:30:41 -0400

    repair main

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from setuptools import setup, find_packages
  
-if __name__=='__main__':
-    setup(name='new',
-          description='esto es un trabajo new',
-          license='MIT',
-          url='https://github.com/lavos90/new.git',
-          version='0.0.1',
-          author='yadnier',
-          author_email='yadnierc@gmail.com',
-          long_description=open('README.md').read(),
-          packages=find_packages(),
-          zip_safe=False,
-          install_requires=[],
-          classifiers=['Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'Natural Language :: English',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programing Language :: Python',
-          ]
-          )
+if __name__=='__master__':
+    setup(
+        name='new',
+        description='esto es un trabajo new',
+        license='MIT',
+        url='https://github.com/lavos90/new.git',
+        version='0.0.1',
+        author='yadnier',
+        author_email='yadnierc@gmail.com',
+        long_description=open('README.md').read(),
+        packages=find_packages(),
+        zip_safe=False,
+        install_requires=[],
+        classifiers=['Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programing Language :: Python',
+        ]
+        )
 
 
 
